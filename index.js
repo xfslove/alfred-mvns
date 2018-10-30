fd94cb7347d27a2fdd355b4f25134ff5609d4c6b
hanwen.huang <xfslove@gmail.com>
2018-10-30 10:49:35 +0800

    fixed #1

--- index.js
+++ index.js
@@ -2,21 +2,33 @@
 const alfy = require('alfy');
 const dateFormat = require('date-format');
 
-const q = alfy.input;
+let q = alfy.input;
+const s = q.split(':');
+
+if (s.length > 1) {
+  q = 'g:'+s[0];
+  if (s[1]) {
+    q = q+' AND '+'a:'+s[1];
+  } 
+  if (s.length > 2 && s[2]) {
+    q = q+' AND '+'v:'+s[2];
+  }
+}
 
 alfy.fetch('http://search.maven.org/solrsearch/select', {
 	query: {
 		q,
-    wt: 'json',
+    start: 0,
 		rows: 20
 	}
 }).then(data => {
   const items = data.response.docs
 	  .map(x => {
-      const mvn = `<dependency>\n  <groupId>${x.g}</groupId>\n  <artifactId>${x.a}</artifactId>\n  <version>${x.latestVersion}</version>\n</dependency>`;
-      const gradle = `compile '${x.g}:${x.a}:${x.latestVersion}'`;
+      const v = x.v?x.v:x.latestVersion;
+      const mvn = `<dependency>\n  <groupId>${x.g}</groupId>\n  <artifactId>${x.a}</artifactId>\n  <version>${v}</version>\n</dependency>`;
+      const gradle = `compile '${x.g}:${x.a}:${v}'`;
 		  return {
-			  title: `${x.g}:${x.a}:${x.latestVersion}`,
+			  title: `${x.g}:${x.a}:${v}`,
 			  subtitle: `updated at ${dateFormat('yyyy-dd-MM', new Date(x.timestamp))}`,
         arg: mvn,
         mods: {
